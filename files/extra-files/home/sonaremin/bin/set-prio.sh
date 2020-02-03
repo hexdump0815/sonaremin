f3944852c82537dd00fd0b0639eebc82692a5be4
hexdump <hexdump0815@googlemail.com>
2020-02-04 00:02:01 +0100

    sometimes set-prio did not find Rack

--- files/extra-files/home/sonaremin/bin/set-prio.sh
+++ files/extra-files/home/sonaremin/bin/set-prio.sh
@@ -6,9 +6,9 @@
 # is done in an endless loop every 15 seconds
 
 while true; do
-  RACK_PID=`ps au | grep -v grep | grep Rack | awk '{print $2}'`
+  RACK_PID=`ps aux | grep -v grep | grep Rack | awk '{print $2}'`
   if [ "${RACK_PID}" != "" ]; then
-    for i in `ps auH -T | grep -v grep | grep Rack | awk '{print $3" "$4}' | grep -v ${RACK_PID} | tail -n 2 | awk '{print $1}'`; do
+    for i in `ps auxH -T | grep -v grep | grep Rack | awk '{print $3" "$4}' | grep -v ${RACK_PID} | tail -n 2 | awk '{print $1}'`; do
       chrt --pid 20 $i
     done
   fi
