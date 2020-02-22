5f1817df947ef7ce3646751986e5b9946b7741c7
hexdump <hexdump0815@googlemail.com>
2020-02-22 10:27:16 +0100

    set prio & affinity for jack too

--- files/extra-files/home/sonaremin/bin/set-rtprio-and-cpu-affinity.sh
+++ files/extra-files/home/sonaremin/bin/set-rtprio-and-cpu-affinity.sh
@@ -10,6 +10,8 @@
 # from cpu cores which are maybe busy with other stuff like interrupt
 # handling (like for instance cpu0 on the raspberry pi 3) or to
 # schedule the audio threads to the big cores in big-little socs
+#
+# the same is done for the realtime relevant jackbus thread
 
 # read the sonaremin config file if it exists, otherwise set default values
 if [ -f /data/config/sonaremin.txt ]; then
@@ -21,12 +23,18 @@ if [ "${DESIRED_CPU_AFFINITY}" = "" ]; then
   DESIRED_CPU_AFFINITY="2,3"
 fi
 
+if [ "${DESIRED_CPU_AFFINITY_JACK}" = "" ]; then
+  # on which cpu cores should jack run - default: 1 (0 = cpu1 etc.)
+  DESIRED_CPU_AFFINITY_JACK="1"
+fi
+
 DESIRED_RT_PRIORITY="50"
 
 # set this script itself to cpu core 0 and unimportant scheduling prio
 taskset -a -pc 1 $$ > /dev/null
 chrt -o -p $$ > /dev/null
 
+# vcvrack
 VCVRACK_PID=`pidof Rack`
 if [ "${VCVRACK_PID}" != "" ]; then
   for i in `top -p ${VCVRACK_PID} -b -n 1 -H | sed 1,7d | grep Engine | awk '{print $1}'`; do
@@ -39,6 +47,23 @@ if [ "${VCVRACK_PID}" != "" ]; then
     if [ "${CURRENT_AFFINITY}" != "${DESIRED_CPU_AFFINITY}" ]; then
       # set cpu affinity
       taskset -pc ${DESIRED_CPU_AFFINITY} $i > /dev/null
+    fi
+  done
+fi
+
+# jack
+JACK_PID=`pidof jackdbus`
+if [ "${JACK_PID}" != "" ]; then
+  for i in `top -p ${JACK_PID} -b -n 1 -H | sed 1,7d | grep '\-11' | awk '{print $1}'`; do
+    CURRENT_PRIORITY=`chrt -p $i | grep priority | sed 's,.*scheduling priority: ,,g'`
+    if [ "${CURRENT_PRIORITY}" != "${DESIRED_RT_PRIORITY}" ]; then
+      # set realtime scheduling priority
+      chrt -p ${DESIRED_RT_PRIORITY} $i
+    fi
+    CURRENT_AFFINITY=`taskset -pc $i | sed 's,.*list: ,,g'`
+    if [ "${CURRENT_AFFINITY}" != "${DESIRED_CPU_AFFINITY_JACK}" ]; then
+      # set cpu affinity
+      taskset -pc ${DESIRED_CPU_AFFINITY_JACK} $i > /dev/null
     fi
   done
 fi
