c4ffdeb37fd278cc665959b155b10ea959d3509e
hexdump <hexdump0815@googlemail.com>
2020-07-24 17:54:08 +0200

    proper dtb name

--- files/data/config/systems/rockchip_rk322x_mk809iv_tv_stick.sh
+++ files/data/config/systems/rockchip_rk322x_mk809iv_tv_stick.sh
@@ -21,7 +21,7 @@ if [ "$?" = "0" ]; then
     ( sleep 15; AUDIO_DEVICE=`aplay -l | grep "DAC \[USB AUDIO    DAC\]" | awk '{print $2}' | sed 's,:,,g'`; if [ "$AUDIO_DEVICE" != "" ]; then amixer -c ${AUDIO_DEVICE} set PCM 64 ; fi ) &
   fi
   echo "SYSTEM_MODEL=rk322x" > /data/config/info.txt
-  echo "SYSTEM_MODEL_DETAILED=mkv809iv_tv_stick" >> /data/config/info.txt
+  echo "SYSTEM_MODEL_DETAILED=mk809iv_tv_stick" >> /data/config/info.txt
   # limit the cpu clock to avoid overheating
   # possible values: cat /sys/devices/system/cpu/cpufreq/policy?/scaling_available_frequencies
   #echo MAX_CPU_CLOCK=1200000 >> /data/config/info.txt
